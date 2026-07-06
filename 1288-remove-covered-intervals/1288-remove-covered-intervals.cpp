d628b10a951c45317985b191e0a3d4b1831745ff
khushi Dimri <khushidimri601@gmail.com>
2026-07-07 01:11:03 +0530

    Time: 1 ms (52.99%), Space: 15.3 MB (30.65%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        auto lambda=[](vector<int>&vec1,vector<int>&vec2){
+            if(vec1[0]==vec2[0])
+              return vec1[1]>vec2[1];
+            return vec1[0]<vec2[0];
+        };
+        sort(begin(intervals),end(intervals),lambda);
+        int IntervalEnd=intervals[0][1];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(IntervalEnd>=intervals[i][1])
+             continue;
+            IntervalEnd=intervals[i][1];
+            count++;
+        }
+        return count;
+    }
+};
