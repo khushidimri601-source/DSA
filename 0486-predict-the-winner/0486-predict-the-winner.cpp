9c7f8eed14c1ca37c15396acf0585010413d3d8f
khushi Dimri <khushidimri601@gmail.com>
2026-08-01 12:56:09 +0530

    Time: 0 ms (100%), Space: 9.5 MB (85.12%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int t[23][23];
+   int solve(int i,int j,vector<int>& nums){
+    if(i>j)
+     return 0;
+    if(i==j)
+     return nums[i];
+    if(t[i][j]!=-1)
+     return t[i][j];
+    int take_i=nums[i]-solve(i+1,j,nums);
+    int take_j= nums[j]-solve(i,j-1,nums);
+    return t[i][j]=max(take_i,take_j);
+
+   }
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,n-1,nums)>=0;
+    }
+};
